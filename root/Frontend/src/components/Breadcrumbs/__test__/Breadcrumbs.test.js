492ea8cb91e6587b0c407af6051d0a7a1e90bff5
= <=>
2023-03-11 21:32:18 -0300

    test: update breadcrumbs component

--- root/Frontend/src/components/Breadcrumbs/__test__/Breadcrumbs.test.js
+++ root/Frontend/src/components/Breadcrumbs/__test__/Breadcrumbs.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
-import {Breadcrumbs} from '../index';
+import { Breadcrumbs } from '../index';
 
-test('renders learn react link', () => {
-  render(<Breadcrumbs categories={['categories','hola']}/>);
-  const linkElement = screen.getAllByRole("listitem");
-  expect(linkElement.length).toBe(2);
+describe("Breadcrumbs component", () => {
+
+    test('should render breadcrumbs list idems', () => {
+        render(<Breadcrumbs categories={['categories', 'hola']} />);
+        const linkElement = screen.getAllByRole("listitem");
+        expect(linkElement.length).toBe(2);
+    });
+
+    test('should not to render Breadcrumbs component', () => {
+        render(<Breadcrumbs categories={[]} />);
+        const linkElement = screen.queryByRole("list");
+        expect(linkElement).not.toBeInTheDocument() ;
+    });
 });
